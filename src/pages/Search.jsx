26669b6e8ced261e8d87588968caeb80ca2ecb27
AndrewDluca <andrews.adlb@gmail.com>
2022-11-29 21:37:01 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,38 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 
 class Search extends Component {
+  state = {
+    buttonLogin: true,
+  };
+
+  loginCheck = (event) => {
+    const { value } = event.target;
+    const minLength = 2;
+    const verify = value.length < minLength;
+
+    this.setState({
+      buttonLogin: verify,
+    });
+  };
+
   render() {
+    const { buttonLogin } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <input
+          type="text"
+          data-testid="search-artist-input"
+          onChange={ this.loginCheck }
+        />
+        <button
+          data-testid="search-artist-button"
+          type="button"
+          disabled={ buttonLogin }
+        >
+          Pesquisar
+        </button>
+
       </div>
     );
   }
