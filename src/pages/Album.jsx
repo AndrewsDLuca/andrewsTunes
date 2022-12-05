9a82f017352b4e81abb89ca376a79a2101e33800
AndrewDluca <andrews.adlb@gmail.com>
2022-12-05 20:58:11 -0300

    requisito 7

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -1,14 +1,51 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import Header from './components/Header';
+import MusicCard from './components/MusicCard';
+import getMusics from '../services/musicsAPI';
 
-class Album extends Component {
+export default class Album extends Component {
+  state = {
+    listMusic: [],
+    musicName: {},
+  };
+
+  async componentDidMount() {
+    const { match } = this.props;
+    const [api, ...listMusic] = await getMusics(match.params.id);
+    this.setState({
+      listMusic,
+      musicName: api,
+    });
+  }
+
   render() {
+    const { listMusic, musicName } = this.state;
     return (
       <div data-testid="page-album">
         <Header />
+        <section>
+          <h2 data-testid="artist-name">
+            { musicName.artistName }
+          </h2>
+          <h2 data-testid="album-name">
+            { musicName.collectionName }
+          </h2>
+        </section>
+        {
+          listMusic.map((music) => (
+            <MusicCard key={ music.trackName } music={ music } { ...this.state } />
+          ))
+        }
       </div>
     );
   }
 }
 
-export default Album;
+Album.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.number,
+    }),
+  }),
+}.isRequired;
