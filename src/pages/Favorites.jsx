baa966fca46995a4e63ad4282cdf85c4828e68c9
AndrewDluca <andrews.adlb@gmail.com>
2022-12-06 02:56:12 -0300

    requisito 12

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,31 +1,61 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
-import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import Loading from './components/Loading';
+import { getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import MusicCard from './components/MusicCard';
 
 export default class Favorites extends Component {
   state = {
     favoriteSongs: [],
+    loading: false,
+    check: {},
   };
 
   async componentDidMount() {
+    this.setState({ loading: true });
     const favoriteSongs = await getFavoriteSongs();
     this.setState({
       favoriteSongs,
+      loading: false,
     });
     console.log(favoriteSongs);
     console.log(this.state);
   }
 
+  onInputChange = ({ target }) => {
+    const { name, check, id } = target;
+    this.setState((prev) => ({
+      check: { ...prev.check, [name]: check },
+    }));
+    if (!check) {
+      this.setState({ loading: true }, async () => {
+        const { favoriteSongs } = this.state;
+        const songGet = favoriteSongs.find((song) => song.trackId === +id);
+        await removeSong(songGet);
+        const getFavorites = await getFavoriteSongs();
+        this.setState({
+          favoriteSongs: getFavorites,
+          loading: false,
+        });
+      });
+    }
+  };
+
   render() {
-    const { favoriteSongs } = this.state;
+    const { favoriteSongs, loading, check } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
         <h3>Músicas favoritas:</h3>
-        {favoriteSongs.map((music) => (
-          <MusicCard key={ music.trackId } music={ music } { ...this.state } />
-        ))}
+        { loading ? <Loading /> : (
+          favoriteSongs.map((music) => (
+            <MusicCard
+              key={ music.trackId }
+              music={ music }
+              checked={ check[music.trackName] }
+              onInputChange={ this.onInputChange }
+            />
+          )))}
       </div>
     );
   }
