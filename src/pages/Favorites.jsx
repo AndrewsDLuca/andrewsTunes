4ff6ca1f0954be2b063ca1a87c9c91d243ca5ffe
AndrewDluca <andrews.adlb@gmail.com>
2022-12-06 02:08:20 -0300

    requisito 13

--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
 import Header from './components/Header';
+import { getFavoriteSongs } from '../services/favoriteSongsAPI';
+import MusicCard from './components/MusicCard';
 
-class Favorites extends Component {
+export default class Favorites extends Component {
+  state = {
+    favoriteSongs: [],
+  };
+
+  async componentDidMount() {
+    const favoriteSongs = await getFavoriteSongs();
+    this.setState({
+      favoriteSongs,
+    });
+    console.log(favoriteSongs);
+    console.log(this.state);
+  }
+
   render() {
+    const { favoriteSongs } = this.state;
     return (
       <div data-testid="page-favorites">
         <Header />
-        aa
+        <h3>Músicas favoritas:</h3>
+        {favoriteSongs.map((music) => (
+          <MusicCard key={ music.trackId } music={ music } { ...this.state } />
+        ))}
       </div>
     );
   }
 }
-
-export default Favorites;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,50 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from './components/Header';
+import Loading from './components/Loading';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  state = {
+    name: '',
+    email: '',
+    description: '',
+    loading: false,
+  };
+
+  async componentDidMount() {
+    this.setState({ loading: true });
+    const naruto = await getUser();
+    this.setState({
+      name: naruto.name,
+      email: naruto.email,
+      description: naruto.description,
+      image: naruto.image,
+      loading: false,
+    });
+  }
+
   render() {
+    const { name, email, description, image, loading } = this.state;
     return (
       <div data-testid="page-profile">
         <Header />
+        {
+          loading ? <Loading /> : (
+            <div>
+              <img
+                src={ image }
+                alt={ name }
+                data-testid="profile-image"
+              />
+              <div />
+              <h2>{name}</h2>
+              <h2>{email}</h2>
+              <h2>{description}</h2>
+              <p><Link to="profile/edit">Editar perfil</Link></p>
+            </div>
+          )
+        }
       </div>
     );
   }
